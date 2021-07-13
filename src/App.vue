303b0c17355a94be815de8e9c52361bb85705da1
victorluismendozalugo <kragsoftware@gmail.com>
2021-07-13 09:43:35 -0600

    se carga el componente cards que muestra el listado de productos del negocio, esta pendiente darle el formato correcto

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,52 @@
 <template>
   <div id="app">
-    <principal hidden="hidden"/>
-    <tablas/>
-    <cards/>
+    <!-- <principal hidden="hidden" />
+    <tablas /> -->
+    <cards :arr="arr" :load="loading" />
   </div>
 </template>
 
 <script>
-import Principal from './components/Principal.vue'
-import Tablas from './components/Tablas.vue'
-import Cards from './components/Cards.vue'
+// import Principal from "./components/Principal.vue";
+// import Tablas from "./components/Tablas.vue";
+import Cards from "./components/Cards.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Tablas,
-    Principal,
-    Cards
-  }
-}
+    // Tablas,
+    // Principal,
+    Cards,
+  },
+  data() {
+    return {
+      arr: [],
+      loading: false,
+    };
+  },
+  mounted() {
+    this.loading = true
+    this.getProds();
+  },
+  methods: {
+    getProds() {
+      var datos = {
+        productoID: 0,
+        productoSucID: 1,
+      };
+      http
+        .postLoader("catalogo/productos", datos)
+        .then((response) => {
+          this.arr = response.data.data.data;
+          console.log(this.arr)
+          this.loading = false
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+  },
+};
 </script>
 
 <style>
--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -4,15 +4,15 @@
       <v-row>
         <v-col
           cols="12"
-          xl="4"
+          xl="2"
           lg="4"
-          md="6"
+          md="8"
           sm="8"
           xs="12"
-          v-for="i in 5"
-          :key="i"
+          v-for="(prod, x) in arr"
+          :key="x"
         >
-          <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+          <v-card :loading="load" class="mx-auto my-12" max-width="374" @click.stop="eventoclick(prod)">
             <template slot="progress">
               <v-progress-linear
                 color="deep-purple"
@@ -21,41 +21,36 @@
               ></v-progress-linear>
             </template>
 
-            <v-img
-              height="250"
-              src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-            ></v-img>
+            <v-img height="250" :src="prod.prodImagen1"></v-img>
 
-            <v-card-title>Cafe Badilico</v-card-title>
+            <v-card-title>{{
+              prod.productoDescCorta.substring(0, 20)
+            }}</v-card-title>
 
             <v-card-text>
               <v-row align="center" class="mx-0">
-                <v-rating
-                  :value="4.5"
-                  color="amber"
-                  dense
-                  half-increments
-                  readonly
-                  size="14"
-                ></v-rating>
-
-                <div class="grey--text ms-4">4.5 (413)</div>
+                <v-col>
+                  <div class="grey--text ms-4">COD: {{ prod.productoCod }}</div>
+                </v-col>
+                <v-col>
+                  <div class="grey--text ms-4">Precio Renta</div>
+                  <div class="my-4 text-subtitle-1">
+                    ${{ prod.prodPrecioRenta }}
+                  </div>
+                </v-col>
               </v-row>
 
-              <div class="my-4 text-subtitle-1">$ • Italian, Cafe</div>
-
               <div>
-                Small plates, salads & sandwiches - an intimate setting with 12
-                indoor seats plus patio seating.
+                {{ prod.productoDescLarga }} Color: {{ prod.colorNombre }}
               </div>
             </v-card-text>
 
             <v-divider class="mx-4"></v-divider>
 
-            <v-card-title>Tonight's availability</v-card-title>
+            <v-card-title>Revisar disponibilidad</v-card-title>
 
             <v-card-text>
-              <v-chip-group
+              <!-- <v-chip-group
                 v-model="selection"
                 active-class="deep-purple accent-4 white--text"
                 column
@@ -67,12 +62,39 @@
                 <v-chip>8:00PM</v-chip>
 
                 <v-chip>9:00PM</v-chip>
-              </v-chip-group>
+              </v-chip-group> -->
+              <v-container>
+                <v-row>
+                  <v-col cols="12" lg="12" xl="12">
+                    <v-menu
+                      v-model="menu1"
+                      :close-on-content-click="false"
+                      max-width="290"
+                    >
+                      <template v-slot:activator="{ on, attrs }">
+                        <v-text-field
+                          :value="computedDateFormattedMomentjs"
+                          clearable
+                          label="Selecciona la fecha de tu evento"
+                          readonly
+                          v-bind="attrs"
+                          v-on="on"
+                          @click:clear="date = null"
+                        ></v-text-field>
+                      </template>
+                      <v-date-picker
+                        v-model="date"
+                        @change="menu1 = false"
+                      ></v-date-picker>
+                    </v-menu>
+                  </v-col>
+                </v-row>
+              </v-container>
             </v-card-text>
 
             <v-card-actions>
               <v-btn color="deep-purple lighten-2" text @click="reserve">
-                Reserve
+                Mas informacion
               </v-btn>
             </v-card-actions>
           </v-card>
@@ -82,23 +104,32 @@
   </div>
 </template>
   <script>
+import moment from "moment";
+import { format, parseISO } from "date-fns";
 export default {
   name: "Cards",
-  data() {
+  props: ["arr", "load"],
+  data: () => {
     return {
-      loading: false,
+      loading: true,
       selection: 1,
+      date: format(parseISO(new Date().toISOString()), "yyyy-MM-dd"),
+      menu1: false,
     };
+  },
+  computed: {
+    computedDateFormattedMomentjs() {
+      return this.date ? moment(this.date).format("dddd, MMMM Do YYYY") : "";
+    }
   },
   methods: {
     reserve() {
       this.loading = true;
-
       setTimeout(() => (this.loading = false), 2000);
     },
+    eventoclick(i){
+      console.log(i)
+    }
   },
 };
 </script>
-  
-  <style>
-</style>
